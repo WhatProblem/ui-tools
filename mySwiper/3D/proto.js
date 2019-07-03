65a177b71c11ae34008bbe00dbb5d2d80b70978b
WhatProblem <18355350915@163.com>
2019-07-03 21:22:36 +0800

    新增3D旋转木马轮播

---
+++ mySwiper/3D/proto.js
@@ -0,0 +1,89 @@
+function Slider3D(option) {
+    this.el = option.el
+    this.inner = option.el.children[0]
+    this.leftBtn = option.el.children[1]['children'][0]
+    this.rightBtn = option.el.children[1]['children'][1]
+    this.json = option.json
+    this.timer = null
+    this.addStyle()
+    this.autoPlay()
+    this.doEvent()
+}
+
+Slider3D.prototype.autoPlay = function () {
+    this.timer = setInterval(() => {
+        this.json.push(this.json.shift())
+        this.addStyle()
+    }, 1000);
+}
+
+Slider3D.prototype.doEvent = function () {
+    this.el.onmouseover = function () {
+        clearInterval(this.timer)
+        this.timer = null
+    }.bind(this)
+    this.el.onmouseout = function () {
+        this.autoPlay()
+    }.bind(this)
+    this.rightBtn.onclick = function () {
+        this.json.push(this.json.shift())
+        this.addStyle()
+    }.bind(this)
+    this.leftBtn.onclick = function () {
+        this.json.unshift(this.json.pop())
+        this.addStyle()
+    }.bind(this)
+}
+
+Slider3D.prototype.addStyle = function () {
+    for (let i = 0, len = this.inner.children.length; i < len; i++) {
+        this.inner.children[i].style.cssText = ((JSON.stringify(this.json[i])).replace(/{|}|"|'/g, '')).replace(/,/g, ';');
+        this.inner.children[i].style.transition = 'all 0.5s';
+    }
+}
+
+let slider = new Slider3D({
+    el: document.getElementById('slider'),
+    json: [
+        { // 图1
+            width: '500px',
+            height: '260px',
+            top: '70px',
+            left: '150px',
+            'z-index': 5,
+            opacity: 1,
+        },
+        { // 图2
+            width: '370px',
+            height: '200px',
+            top: '50px',
+            left: '20px',
+            'z-index': 4,
+            opacity: 0.8,
+        },
+        { // 图3
+            width: '270px',
+            height: '150px',
+            top: '20px',
+            left: '50px',
+            'z-index': 3,
+            opacity: 0.4,
+        },
+        { // 图4
+            width: '270px',
+            height: '150px',
+            top: '20px',
+            right: '50px',
+            'z-index': 1,
+            opacity: 0.4,
+        },
+        { // 图5
+            width: '370px',
+            height: '200px',
+            top: '50px',
+            right: '20px',
+            'z-index': 2,
+            opacity: 0.8,
+        },
+    ]
+})
--- mySwiper/README.md
+++ mySwiper/README.md
@@ -20,4 +20,62 @@ animate(obj, target) {
 }
 ```
 
-2.后续持续更新其他~~~
+2.3D旋转木马轮播图
+```
+    主要思路：利用数组的pop(),push(),shift(),unshift()方法，通过改变数组元素顺序，配合CSS3过渡实现轮播效果
+```
+```javascript
+// 关键函数
+addStyle() {
+    for (let i = 0, len = this.inner.children.length; i < len; i++) {
+        this.inner.children[i].style.cssText = ((JSON.stringify(this.json[i])).replace(/{|}|"|'/g, '')).replace(/,/g, ';');
+        this.inner.children[i].style.transition = 'all 0.5s';
+    }
+}
+
+// 数组结构
+[
+    { // 图1
+        width: '500px',
+        height: '260px',
+        top: '70px',
+        left: '150px',
+        'z-index': 5,
+        opacity: 1,
+    },
+    { // 图2
+        width: '370px',
+        height: '200px',
+        top: '50px',
+        left: '20px',
+        'z-index': 4,
+        opacity: 0.8,
+    },
+    { // 图3
+        width: '270px',
+        height: '150px',
+        top: '20px',
+        left: '50px',
+        'z-index': 3,
+        opacity: 0.4,
+    },
+    { // 图4
+        width: '270px',
+        height: '150px',
+        top: '20px',
+        right: '50px',
+        'z-index': 1,
+        opacity: 0.4,
+    },
+    { // 图5
+        width: '370px',
+        height: '200px',
+        top: '50px',
+        right: '20px',
+        'z-index': 2,
+        opacity: 0.8,
+    },
+]
+```
+
+后续持续更新其他~~~
